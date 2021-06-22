1d8305ddbb60a20bf83befe5135077f0f67ab147
WolfgangWolfgang <86309062+WolfgangWolfgang@users.noreply.github.com>
2021-06-22 16:58:42 +0200

    Ich habe als Test etwas zur XAML Hinzugefügt
    "Das ist en Test"

--- Inf2_P5_Roulette/obj/Debug/netcoreapp3.1/App.g.i.cs
+++ Inf2_P5_Roulette/obj/Debug/netcoreapp3.1/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Inf2_P5_Roulette {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Inf2_P5_Roulette {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public static void Main() {
             Inf2_P5_Roulette.App app = new Inf2_P5_Roulette.App();
             app.InitializeComponent();
