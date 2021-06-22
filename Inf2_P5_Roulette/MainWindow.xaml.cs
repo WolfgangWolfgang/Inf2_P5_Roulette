a8bf3adf31f1773e48c4b65dc9229ff4dfd25327
WolfgangWolfgang <86309062+WolfgangWolfgang@users.noreply.github.com>
2021-06-22 19:14:07 +0200

    Menüs
    Hinzufügen von 2 Menüs

--- Inf2_P5_Roulette/MainWindow.xaml.cs
+++ Inf2_P5_Roulette/MainWindow.xaml.cs
@@ -24,5 +24,65 @@ namespace Inf2_P5_Roulette
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Ruft in einem neuen Fenster die Bestenliste der ersten n - besten Spiele auf 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuBestenListe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Menu: Ruft in einem neuen Fenster Die Hilfe / Spielanleitung auf
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuHilfe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Eventuell Unnötig, jenach Benutzung zu Entfernen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Menu: Beendet Sofort das gesamte Programm
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Schwierigkeitsgrad: Setzt den Schwierigkeitsgrad auf Normal <-- Alles zufällig
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuNormal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Schwierigkeitsgrad: Setzt den Schwirigkeitsgrad auf Unbesiegbar <-- Gegner setzt nach dem Spieler und Tippt IMMER richtig!
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mnuUnbesiegbar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
